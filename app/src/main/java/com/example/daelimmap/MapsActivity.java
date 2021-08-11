977eb7bd57d364dd39ac71d3c56b3d90d4297a82
dlwndrhs05 <dlwndrhs05@gmail.com>
2021-08-11 15:09:52 +0900

    revert

--- app/src/main/java/com/example/daelimmap/MapsActivity.java
+++ app/src/main/java/com/example/daelimmap/MapsActivity.java
@@ -1,10 +1,9 @@
 package com.example.daelimmap;
 
-import android.app.SearchManager;
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
@@ -17,9 +16,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.daelimmap.Intenttt.*;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback{
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,GoogleMap.OnMarkerClickListener,GoogleMap.OnMapClickListener{
+
+    View v;
+    ImageView imageView;
     private GoogleMap mMap;
 
     LatLng Toegyegwan = new LatLng(37.403268056034186, 126.9306871521674); //퇴계관
@@ -36,8 +42,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     LatLng imgoggwan = new LatLng(37.40392495963885, 126.93109889545208); //임곡관
     LatLng hanlimgwan = new LatLng(37.402209544693086, 126.92890403822585); //한림관
 
-    LatLng [] wido = {Toegyegwan,yulgoggwan,UniversityHeadquarters,
-            jeongbotongsingwan,jeonsangwan,saenghwalgwan, hongjigwan, hagsaenghoegwan,jadongchagwan,suamgwan,dasangwan, imgoggwan,hanlimgwan};// 각 건물의 위도와 경도
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,54 +55,100 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        SV.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
     }
+
+
     @Override
     public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
         MarkerOptions markerOptions = new MarkerOptions();  // 마커 생성
-        markerOptions.visible(false);
 
-        for(int idx =0; idx<wido.length;idx++) {
 
-            mMap.addMarker(markerOptions.position(Toegyegwan).title("퇴계관"));
-            mMap.addMarker(markerOptions.position(yulgoggwan).title("율곡관"));
-            mMap.addMarker(markerOptions.position(UniversityHeadquarters).title("대학본부"));
-            mMap.addMarker(markerOptions.position(jeongbotongsingwan).title("정보통신관"));
-            mMap.addMarker(markerOptions.position(jeonsangwan).title("전산관"));
-            mMap.addMarker(markerOptions.position(saenghwalgwan).title("생활관"));
-            mMap.addMarker(markerOptions.position(hongjigwan).title("홍지관"));
-            mMap.addMarker(markerOptions.position(hagsaenghoegwan).title("학생회관"));
-            mMap.addMarker(markerOptions.position(jadongchagwan).title("자동차관"));
-            mMap.addMarker(markerOptions.position(suamgwan).title("수암관"));
-            mMap.addMarker(markerOptions.position(dasangwan).title("다산관"));
-            mMap.addMarker(markerOptions.position(imgoggwan).title("임곡관"));
-            mMap.addMarker(markerOptions.position(hanlimgwan).title("한림관"));
-        }
+        mMap.addMarker(markerOptions.position(Toegyegwan).title("퇴계관").alpha(0));
+        mMap.addMarker(markerOptions.position(yulgoggwan).title("율곡관").alpha(0));
+        mMap.addMarker(markerOptions.position(UniversityHeadquarters).title("대학본부").alpha(0));
+        mMap.addMarker(markerOptions.position(jeongbotongsingwan).title("정보통신관").alpha(0));
+        mMap.addMarker(markerOptions.position(jeonsangwan).title("전산관").alpha(0));
+        mMap.addMarker(markerOptions.position(saenghwalgwan).title("생활관").alpha(0));
+        mMap.addMarker(markerOptions.position(hongjigwan).title("홍지관").alpha(0));
+        mMap.addMarker(markerOptions.position(hagsaenghoegwan).title("학생회관").alpha(0));
+        mMap.addMarker(markerOptions.position(jadongchagwan).title("자동차관").alpha(0));
+        mMap.addMarker(markerOptions.position(suamgwan).title("수암관").alpha(0));
+        mMap.addMarker(markerOptions.position(dasangwan).title("다산관").alpha(0));
+        mMap.addMarker(markerOptions.position(imgoggwan).title("임곡관").alpha(0));
+        mMap.addMarker(markerOptions.position(hanlimgwan).title("한림관").alpha(0));
 
-            mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                @Override
-                public void onMapClick( LatLng latLng) {
-                    markerOptions.visible(true);
-                    markerOptions.position(latLng);
-
-                      mMap.clear();
-                      mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-                      mMap.addMarker(markerOptions);
-                }
-            });
         mMap.moveCamera(CameraUpdateFactory.newLatLng(yulgoggwan));                 // 초기 위치
         mMap.animateCamera(CameraUpdateFactory.zoomTo(17));                         // 줌의 정도
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);                           // 지도 유형 설정
+
+
+        mMap.setOnMarkerClickListener(this);
+        mMap.setOnMapClickListener(this);
+
+
+    }
+
+
+
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+
+
+        marker.setAlpha(1);
+
+
+        if(marker.getTitle().equals("한림관")){
+            Intent intent = new Intent(getApplicationContext(), hanlimgwan.class);
+            startActivity(intent);
+        }
+
+        return false;
+
+
+    }
+
+
+    @Override
+    public void onMapClick( LatLng latLng) {
+        ArrayList<LatLng> list = new ArrayList<LatLng>();
+
+        list.add(Toegyegwan);
+        list.add(yulgoggwan);
+        list.add(UniversityHeadquarters);
+        list.add(jeongbotongsingwan);
+        list.add(jeonsangwan);
+        list.add(saenghwalgwan);
+        list.add(hongjigwan );
+        list.add(hagsaenghoegwan);
+        list.add(jadongchagwan);
+        list.add(suamgwan);
+        list.add(dasangwan);
+        list.add(imgoggwan);
+        list.add(hanlimgwan);
+
+
+
+
     }
 }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the options menu from XML
+//        MenuInflater inflater = getMenuInflater();
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//
+//        // Get the SearchView and set the searchable configuration
+//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+//        SearchView searchView = (SearchView) menu.findItem(R.id.mapSearch).getActionView();
+//        // Assumes current activity is the searchable activity
+//        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+//        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+//
+//        return true;
+//    }
+
+
+
+
