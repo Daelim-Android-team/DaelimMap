cf5455eefad0c4119fdef123736574fea92fb2b3
ksm <tntn211>
2021-08-16 11:39:17 +0900

    대학본부데이터 입력

--- app/src/main/java/com/example/daelimmap/building/UniversityHeadquarters.java
+++ app/src/main/java/com/example/daelimmap/building/UniversityHeadquarters.java
@@ -1,8 +1,11 @@
 package com.example.daelimmap.building;
+import java.util.Arrays;
+import java.util.List;
 
 public class UniversityHeadquarters {
-//    String [] B_f {};
-//    String [] One_f {};
-//    String [] Two_f {};
-//    String [] Three_f {};
+      List <String>  One_f  = Arrays.asList ("입학실", "당직실", "교육행정팀");
+      List <String>  two_f  = Arrays.asList ("사무 운영팀", "사무 처장실", "총장실", "부속실", "기획처장실", "정착기획팀");
+      List <String>  three_f  = Arrays.asList ( "신학 협력 처장실", "신학 협력 부단장실", "특성화 협력단", "신혁협력단 (inno – café)",
+                                                "대회의실", "소회의실", "창의 융합 우수 작품관");
+      List <String>  four_f  = Arrays.asList ( "혁신 지원 사업단", "혁신 지원 부단장", "마이스터대 사업대", "LINC+ 사업단", "LINC+ 사업단장", "지식 정보 원장실", "지식 정보원");
 }
