9c33112500dea45b5968c15534c053c99156982b
ksm <tntn211>
2021-08-16 12:45:17 +0900

    임곡관, 자동차관 데이터 입력

--- app/src/main/java/com/example/daelimmap/building/Jadongchagwan.java
+++ app/src/main/java/com/example/daelimmap/building/Jadongchagwan.java
@@ -1,12 +1,17 @@
 package com.example.daelimmap.building;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Jadongchagwan {
-//    String [] B_f {};
-//    String [] One_f {};
-//    String [] Two_f {};
-//    String [] Three_f {};
-//    String [] Four_f {};
-//    String [] Five_f {};
-//    String [] Six_f {};
-//    String [] Seven_f{};
+    List<String> One_f = Arrays.asList("자동차과 설비실", "L0105 강의실");
+    List <String> Two_f = Arrays.asList("샤워실");
+    List <String> Three_f = Arrays.asList("정비실습실", "강의실", "기관 실습실", "전시관" );
+    List <String> Four_f = Arrays.asList("이승용 교수님 연구실", "이정호 교수님 연구실", "함성훈 교수님 연구실", "국창호 교수님 연구실",
+            "자동차 학부 학과 사무실", "김필수 교수님 연구실", "첨단 강의실", "전자제어 실습실", "전기 실습실", "L0413 강의실", "L0417 강의실");
+    List <String> Five_f = Arrays.asList("겸임 교수님 연구실", "강호인 교수님 연구실", "이영숙 교수님 연구실", "이일권 교수님 연구실", "휴게실", "박종건 교수님 연구실",
+            "전공 동아리실", "카티아 실습실", "세미나실", "L0511 강의실", "L0515 강의실");
+    List <String> Six_f = Arrays.asList("의전 경호 전공 학과 사무실", "L0610 강의실", "L0602 강의실", "학적 자료실", "진단 실습실", "섀시 실습실", "L0605 강의실", "L0601 강의실");
+    List <String> Seven_f = Arrays.asList("상담실", "평생 교육 지원팀", "L0702 강의실", "학습자 휴게실", "평생 교육 원장실",
+                                            "교강사 휴게실", "L0709 강의실", "L0705 강의실", "L0701 강의실");
 }
