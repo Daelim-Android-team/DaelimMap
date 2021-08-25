30e3d954c54c2df1c40634252148fc63a85f0489
jeonseunghyeon <klzxklzx123@naver.com>
2021-08-25 22:12:26 +0900

    SADSADASD

--- app/src/main/java/com/example/daelimmap/bottomsheet/result.java
+++ app/src/main/java/com/example/daelimmap/bottomsheet/result.java
@@ -1,5 +1,6 @@
 package com.example.daelimmap.bottomsheet;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,5 +13,8 @@ public class result extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        ActionBar actionBar = getSupportActionBar(); //액션바 가리기기
+         actionBar.hide();
     }
 }
