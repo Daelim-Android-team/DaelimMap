f47a9778c04ce635e7ada78cac3f0a35a7a1827d
ksm <tntn211>
2021-08-26 04:25:35 +0900

    퇴계관

--- app/src/main/java/com/example/daelimmap/building/Yulgoggwan.java
+++ app/src/main/java/com/example/daelimmap/building/Yulgoggwan.java
@@ -9,14 +9,14 @@ public class Yulgoggwan {
     List<String> One_f = Arrays.asList("창고", "토목 공용 실험실", "신재생 에너지 실습실", "Ace Factory 1", "Ace Factory 2", "Ace Factory 3");
     List<String> Two_f = Arrays.asList("글로벌 라운지", "어학강의실 1", "어학강의실 2", "멀티미디어실 1", "멀티미디어실 2", "어학강의실 3", "어학강의실 4", "어학강의실 5", "글로벌 라운지 2");
     List<String> Three_f = Arrays.asList("기초 실습실", "시퀀스 실습실", "F0314 강의실", "F0318 강의실", "F0322 강의실", "전력기기 실습실", "전기과 사무실", "석복열 교수님 연구실", "이승요 교수님 연구실",
-                    "김형락 교수님 연구실", "강진규 교수님 연구실", "김규정 교수님 연구실", "전산 실습실", "전기전자 실습실", "창고");
+                                        "김형락 교수님 연구실", "강진규 교수님 연구실", "김규정 교수님 연구실", "전산 실습실", "전기전자 실습실", "창고","조선호 교수님 연구실");
     List<String> Four_f = Arrays.asList("자동차 S/W 응용 실습실","마이크로프로세서 실습실",  "F0414 강의실", "F0418 강의실", "전기 전자 실습실", "센서 및 PLC실습실", "권용관 교수님 연구실", "김상용 교수님 연구실",
                                          "이승 교수님 연구실", "이영옥 교수님 연구실", "심일주 교수님 연구실", "이성재 교수님 연구실",  "F0413 강의실", "English Lounge 2", "양경태 교수님 연구실", "지승한 교수님 연구실", "최동엽 교수님 연구실",
                                         "김태욱 교수님 연구실", "안영기 교수님 연구실", "김광수 교수님 연구실", "반도체 장비 실습실");
     List<String> Five_f = Arrays.asList("시퀀스/PLC 실습실", "산업 전자 응용실습실", "창고", "전공 동아리실", "CAD 실습실", "자동차 기구 및 전산응용실", "재료실","전공 동아리실",
-                                        "기초 회로 제어 실습실", "자동화 시스템 제어 실습실", "자동화 시스템 메카트로닉스", "유공압실습실", "로봇 및 FMS실");
+                                        "기초 회로 제어 실습실", "자동화 시스템 제어 실습실", "자동화 시스템 메카트로닉스", "유공압실습실", "로봇 및 FMS실", "스마트 펙토리 과 사무실");
     List<String> Six_f = Arrays.asList("베이킹&페이스트리 1", "컴퓨터실", "여자 탈의실", "커피 바리스타 실습실", "창고", "커피 바리스타 실습실 2", "남자 탈의실 2", "베이킹&페이스트리 2", "김종욱 교수님 연구실", "최민수 교수님 연구실", "서민수 교수님 연구실",
-                                        "이봉식 교수님 연구실", "김태현 교수님 연구실", "최우승 교수님 연구실",  "강의실 F0621", "강의실 F0625", "워크인 1", "남자 탈의실 1");
+                                        "이봉식 교수님 연구실", "김태현 교수님 연구실", "최우승 교수님 연구실",  "강의실 F0621", "강의실 F0625", "워크인 1", "남자 탈의실 1", "카페 ACE");
     List<String> Seven_f = Arrays.asList("레스토랑 키친", "레스토랑 홀", "키친5", "성기협 교수님 연구실", "글로벌 조리과 사무실", "키친1", "키친2", "키친3", "키친4", "데모강의실");
     public List<List<String>> Ygflooer = Arrays.asList(One_f, Two_f, Three_f, Four_f, Five_f,Six_f,Seven_f);
 
@@ -25,18 +25,18 @@ public class Yulgoggwan {
                                         R.drawable.yulgoggwan_2f_eohagsil3_f0217, R.drawable.yulgoggwan_2f_eohagsil4_f0221, R.drawable.yulgoggwan_2f_eohagsil5_f0223, R.drawable.yulgoggwan_2f_globallounge2);
     List<Integer> Three_p = Arrays.asList(R.drawable.yulgoggwan_3f_gichosilseubsil_f0302,R.drawable.yulgoggwan_3f_sikwonseusilseubsil_f0308,R.drawable.yulgoggwan_3f_ganguisil_f0314, R.drawable.yulgoggwan_3f_ganguisil_f0318, R.drawable.yulgoggwan_3f_ganguisil_f0322, R.drawable.yulgoggwan_3f_jeonlyeoggigisilseubsil_f0326,
                                             R.drawable.yulgoggwan_3f_jeongigwasamusil_f0303,R.drawable.yulgoggwan_3f_sukbokryulgyosuyeongusil_f0305,R.drawable.yulgoggwan_3f_leeseungyogyosuyeongusil_f0307,R.drawable.yulgoggwan_3f_kimhyungrakgyosuyeongusil_f0311,
-                                            R.drawable.yulgoggwan_3f_kangjinkyugyosuyeongusil_f0313,R.drawable.yulgoggwan_3f_kimkyujunggyosuyeongusil_f0315,R.drawable.yulgoggwan_3f_jeonsansilseubsil_f0317, R.drawable.yulgoggwan_3f_jeongigwasamusil_f0303,R.drawable.yulgoggwan_3f_changgo_f0331);
+                                            R.drawable.yulgoggwan_3f_kangjinkyugyosuyeongusil_f0313,R.drawable.yulgoggwan_3f_kimkyujunggyosuyeongusil_f0315,R.drawable.yulgoggwan_3f_jeonsansilseubsil_f0317, R.drawable.yulgoggwan_3f_jeongigwasamusil_f0303,R.drawable.yulgoggwan_3f_changgo_f0331,R.drawable.yulgoggwan_3f_chosunhogyosuyeongusil_f0309);
     List<Integer> Four_p = Arrays.asList(R.drawable.yulgoggwan_4f_jadongchasoftwareeungyongsilseubsil_f0402, R.drawable.yulgoggwan_4f_microprocessoreungyongsilseubsil_f0412,  R.drawable.yulgoggwan_4f_ganguisil_f0414,  R.drawable.yulgoggwan_4f_ganguisil_f0418,  R.drawable.yulgoggwan_4f_jeongijeonjasilseubsil_f0422,  R.drawable.yulgoggwan_4f_sensormichplcsilseubsil_f0428,
                                         R.drawable.yulgoggwan_4f_kwonyongkwangyosuyeongusil_f0401,  R.drawable.yulgoggwan_4f_kimsangyonggyosuyeongusil_f0403,  R.drawable.yulgoggwan_4f_leeseongjaegyosuyeongusil_f0411,  R.drawable.yulgoggwan_4f_leeyoungokgyosuyeongusil_f0407,  R.drawable.yulgoggwan_4f_shimiljugyosuyeongusil_f0409,
                                         R.drawable.yulgoggwan_4f_leeseongjaegyosuyeongusil_f0411,  R.drawable.yulgoggwan_4f_ganguisil_f0413,  R.drawable.yulgoggwan_4f_englishlounge2,  R.drawable.yulgoggwan_4f_yanggyungtaekgyosuyeongusil_f0417,  R.drawable.yulgoggwan_4f_jiseunghangyosuyeongusil_f0419,  R.drawable.yulgoggwan_4f_choidongyubgyosuyeongusil_f0421,
                                         R.drawable.yulgoggwan_4f_kimtaewookgyosuyeongusil_f0423,  R.drawable.yulgoggwan_4f_anyoungkigyosuyeongusil_f0425,  R.drawable.yulgoggwan_4f_kimkwangsugyosuyeongusil_f0427,  R.drawable.yulgoggwan_4f_bandochejangbisilseubsil_f0429);
     List<Integer> Five_p = Arrays.asList(R.drawable.yulgoggwan_5f_sequencemichplcsilseubsil_f0502, R.drawable.yulgoggwan_5f_saneobjeonjaeungyongsilseubsil_f0508, R.drawable.yulgoggwan_5f_changgo_f0514, R.drawable.yulgoggwan_5f_jeongongdongalisil_f0532, R.drawable.yulgoggwan_5f_cadsil_f0518, R.drawable.yulgoggwan_5f_jadonghwagigumichjeonsaneungyongsil_f0524,
                                         R.drawable.yulgoggwan_5f_jaelyosil_f0530,R.drawable.yulgoggwan_5f_jeongongdongalisil_f0532, R.drawable.yulgoggwan_5f_gichohoelojeeoeungyongsilseubsil_f0501, R.drawable.yulgoggwan_5f_jadonghwasiseutemjeeogyoyugsenteo_f0511,
-                                        R.drawable.yulgoggwan_5f_mekateuloniggwasamusil_f0519, R.drawable.yulgoggwan_5f_yugongabsilseubsil_f0521, R.drawable.yulgoggwan_5f_robotmichfmssil_f0527);
+                                        R.drawable.yulgoggwan_5f_mekateuloniggwasamusil_f0519, R.drawable.yulgoggwan_5f_yugongabsilseubsil_f0521, R.drawable.yulgoggwan_5f_robotmichfmssil_f0527, R.drawable.yulgoggwan_5f_jadonghwasiseutemgwasamusil_f0515);
     List<Integer> Six_p = Arrays.asList(R.drawable.yulgoggwan_6f_bakingandpastry1_f0602, R.drawable.yulgoggwan_6f_computersil_f0606, R.drawable.yulgoggwan_6f_yeojataluisil_f0610, R.drawable.yulgoggwan_6f_coffeebarista_f0612, R.drawable.yulgoggwan_6f_changgo_f0616, R.drawable.yulgoggwan_6f_coffeebarista2,
                                         R.drawable.yulgoggwan_6f_namjataluisil2_f0631, R.drawable.yulgoggwan_6f_bakingandpastry2_f0601, R.drawable.yulgoggwan_6f_kimjongwookgyosuyeongusil_f0605, R.drawable.yulgoggwan_6f_choiminsugyosuyeongusil_f0607, R.drawable.yulgoggwan_6f_seominsukgyosuyeongusil_f0609,
                                         R.drawable.yulgoggwan_6f_leebongsikgyosuyeongusil_f0611, R.drawable.yulgoggwan_6f_kimtaehyungyosuyeongusil_f0613, R.drawable.yulgoggwan_6f_choiwooseunggyosuyeongusil_f0615,
-                                        R.drawable.yulgoggwan_6f_ganguisil_f0621, R.drawable.yulgoggwan_6f_ganguisil_f0625,R.drawable.yulgoggwan_6f_walkin1_f0629, R.drawable.yulgoggwan_6f_namjataluisil1_f0626);
+                                        R.drawable.yulgoggwan_6f_ganguisil_f0621, R.drawable.yulgoggwan_6f_ganguisil_f0625,R.drawable.yulgoggwan_6f_walkin1_f0629, R.drawable.yulgoggwan_6f_namjataluisil1_f0626, R.drawable.yulgoggwan_6f_cafeace);
     List<Integer> Seven_p = Arrays.asList(R.drawable.yulgoggwan_7f_restaurantkitchen_f0702, R.drawable.yulgoggwan_7f_restauranthall_f0704, R.drawable.yulgoggwan_7f_kitchen5_f0708, R.drawable.yulgoggwan_7f_sungkihyubgyosuyeongusil_f0712,R.drawable.yulgoggwan_7f_geullobeoljolijegwahagbusamusil,
                                         R.drawable.yulgoggwan_7f_kitchen1_f0703,R.drawable.yulgoggwan_7f_kitchen2_f0707,R.drawable.yulgoggwan_7f_kitchen3_f0711, R.drawable.yulgoggwan_7f_kitchen4_f0715, R.drawable.yulgoggwan_7f_demoganguisil_f0718);
 
